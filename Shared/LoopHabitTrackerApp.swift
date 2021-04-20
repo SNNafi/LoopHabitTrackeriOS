f93a7a340849fbdb1397dc3d823f7f3d0fac1489
Shahriar Nasim Nafi <shahriarnasim.nafi@gmail.com>
2021-04-20 07:22:41 +0600

    Initial Commit

---
+++ Shared/LoopHabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoopHabitTrackerApp.swift
+//  Shared
+//
+//  Created by Shahriar Nasim Nafi on 20/4/21.
+//
+
+import SwiftUI
+
+@main
+struct LoopHabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
